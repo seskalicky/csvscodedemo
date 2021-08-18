a35a081f805644a8706a21c0dcc8a57804046cbe
Simon <seskalicky@gmail.com>
2021-08-18 10:32:10 +1000

    removed some code

--- script.py
+++ script.py
@@ -6,19 +6,11 @@ import requests
 # name = input('Your name? ')
 # print("Hello", name)
 
-print(sys.version)
-print(sys.executable)
 
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-def new_func():
-    test = "test"
 
 
 # print(greet('World'))
 # print(greet('Simon'))
 r = requests.get("https://drsimonskalicky.com.au")
 print(r.status_code)
+print(r.ok)
